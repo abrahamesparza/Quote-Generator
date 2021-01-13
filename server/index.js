7df36fb549f84a89829010c6cb45885eb5e0d475
Abraham Esparza <abrahamesparza.us@gmail.com>
2021-01-12 21:21:32 -0800

    connect db to server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const cors = require('cors');
 const port = 1996;
+const db = require('../database/db.js')
 
 const app = express();
 app.use(bodyParser.json());
