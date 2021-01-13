71e6b4190a47a7641ed371c0c3ecea40c303b342
Abraham Esparza <abrahamesparza.us@gmail.com>
2021-01-12 20:27:31 -0800

    Create server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const cors = require('cors');
+const port = 1996;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static(path.join('public')));
+app.use(cors);
+
+app.get('/', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
