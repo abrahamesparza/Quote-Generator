eb4214391a401db953dce9f1aebe7ff49a6e6059
Abraham Esparza <abrahamesparza.us@gmail.com>
2021-01-24 15:39:24 -0800

    Working on resolving post data issue

--- client/components/ModalForm.js
+++ client/components/ModalForm.js
@@ -1,30 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './modal.css'
+
 
 const ModalForm = ({ show }) => {
   if (!show) {
     return null;
   }
+  let [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    age: ''
+  });
+
+  let handleChange = (e) => {
+    console.log('event name', e.target.name, 'event value', e.target.value);
+    setUser({
+      [e.target.name]: e.target.value
+    });
+  };
+
+
+  let handleSubmit = (e) => {
+    e.preventDefault()
+    let data = user;
+    setUser({[e.target.name]: e.target.value});
+    axios.post('/new/user', data)
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+  }
+
   return (
     <div className='form-container'>
-      <form className='form-space'>
+      <form className='form-space' onSubmit={handleSubmit}>
         <label>
           First Name:<br/>
-          <input type='text' name='firstName'/>
+          <input type='text' name='firstName' onChange={handleChange}/>
         </label><br/>
         <label>
           Last Name:<br/>
-          <input type='text' name='lastName'/>
+          <input type='text' name='lastName' onChange={handleChange}/>
         </label><br/>
         <label>
           Email:<br/>
-          <input type='text' name='email'/>
+          <input type='email' name='email' onChange={handleChange}/>
         </label><br/>
         <label>
           Password:<br/>
-          <input type='text' name='password'/>
+          <input type='password' name='password' onChange={handleChange}/>
         </label><br/>
-        <input type='submit' value='Submit'/>
+        <label>
+          Age:<br/>
+          <input type='text' name='age' onChange={handleChange}/>
+        </label><br/>
+        <input type='submit' value='Submit' />
       </form>
     </div>
   )
