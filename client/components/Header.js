b588624968102de332e29d56e33a0fcb0fc96f9a
Abraham Esparza <abrahamesparza.us@gmail.com>
2021-02-01 20:27:05 -0800

    working on using react router

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import '../styles/header.css';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 const Header = ( { showModal } ) => {
   return (
-    <header>
-      <div>Home</div>
-      <div>About</div>
-      <div onClick={showModal}>Join</div>
-    </header>
+    <Router>
+      <header>
+        <Link to='/'>
+          <div>Home</div>
+        </Link>
+        <Link to='/about'>
+          <div>About</div>
+        </Link>
+        <Link to='/join'>
+          <div onClick={showModal}>Join</div>
+        </Link>
+      </header>
+    </Router>
   )
 };
 
