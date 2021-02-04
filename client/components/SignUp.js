8baec66ac56c11b8148bd7c55a3827c13f593c95
Abraham Esparza <abrahamesparza.us@gmail.com>
2021-02-03 20:14:26 -0800

    build signup skeleton

--- client/components/SignUp.js
+++ client/components/SignUp.js
@@ -3,10 +3,30 @@ import React from 'react';
 const SignUp = () => {
   return (
     <form className='signupForm'>
-    <label>
-      First Name:
-    </label>
+      <label>
+        First Name:
+      </label>
       <input type='text' name='firstName' />
+
+      <label>
+      Last Name:
+      </label>
+      <input type='text' name='firstName' />
+
+      <label>
+      Email:
+      </label>
+      <input type='email' name='firstName' />
+
+      <label>
+      Password:
+      </label>
+      <input type='password' name='firstName' />
+
+      <label>
+      Confirm Password:
+      </label>
+      <input type='password' name='firstName' />
     </form>
   )
 }
