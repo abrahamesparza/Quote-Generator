a591b686728a50ec5e1f6b484a0b2bab71d7c400
Abraham Esparza <abrahamesparza.us@gmail.com>
2021-02-08 20:44:05 -0800

    add login route + user authentication

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ const saltRounds = 10;
 const port = process.env.PORT || 3003;
 
 const db = require('../database/db.js')
-const { Users } = require('../database/loginSchema.js');
+const { Users } = require('../database/signupSchema.js');
+const { NewUser } = require('../database/loginSchema.js');
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -23,7 +25,6 @@ app.get('/', (req, res) => {
 app.get('/users', (req, res) => {
   Users.find((err, data) => {
     console.log('existing users:', data);
-
     if (err) res.status(500).send(err);
     else res.status(200).send(data);
   });
@@ -35,10 +36,29 @@ app.post('/new/user', (req, res) => {
     userData.password = hash;
     if (err) console.log('Error hashing', err);
     Users.create(userData, (err, data) => {
+      console.log('created user:', data);
       if (err) res.status(500).send(err);
       else res.status(201).send(data);
     });
   });
+});
+
+app.post('/login/user', (req, res) => {
+  let userData = req.body;
+  Users.findOne({ email: userData.email })
+  .then(user => {
+    if (!user) res.status(400).json({message: 'user does not exist'})
+    else {
+      bcrypt.compare(userData.password, user.password, (err, data) => {
+        if (err) console.log('err', err);
+        else if (data) {
+          res.status(200).json({message: 'login verified!'})
+        } else {
+          res.status(400).json({message: 'invalid password'})
+        }
+      })
+    }
+  })
 });
 
 app.listen(port, () => {
