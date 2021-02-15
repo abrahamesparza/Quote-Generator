6d2c81ee1218bdd3d264540aebd1392459daa658
Abraham Esparza <abrahamesparza.us@gmail.com>
2021-02-14 21:10:36 -0800

    add favorite quotes to side bar, updates upon favorite click

--- client/components/Aside.js
+++ client/components/Aside.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/aside.css'
+import axios from 'axios';
 
 const Aside = () => {
+  let [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    getFavorites();
+  }, [favorites]);
+
+  let getFavorites = () => {
+    axios.get('/favorites')
+    .then(res => {
+      console.log('GET res:', res)
+      setFavorites(res.data);
+    })
+    .catch(err => console.error(err))
+  }
+  console.log('favorite quotes', favorites);
+
   return (
     <aside>
     <h2>Favorite Quotes</h2>
     <div className='asideBody'>
       {/* dummy data for visual */}
       <ul>
-        <li>dummy</li>
-        <li>data</li>
-        <li>for</li>
-        <li>visualization</li>
+        {favorites.map(quote => {
+          {/* need to consider string length so quote doesnt overlap side bar */}
+          if (quote.text.length > 20) {
+            let fav = quote.text.slice(0, 50);
+            return <li>{fav}</li>
+          }
+        })}
       </ul>
     </div>
     </aside>
