62cbf04d67918170a1a1e234e2f263c6bd7c7f88
Abraham Esparza <abrahamesparza.us@gmail.com>
2021-03-03 20:56:00 -0800

    update css

--- client/components/About.js
+++ client/components/About.js
@@ -10,8 +10,8 @@ const About = () => {
         <p>It is easy to become distracted in our day of age. So many sites are filled with advertisements and algoithms to influence our way of thinking.</p>
         <p>This app is intended to filter out the garbage. Now, you can discover quotes from various people throughout our history without having to be bombarded by ads and algorithm tricks.</p>
         <span></span>
-        <h2>This app was built using:</h2>
-        <ul>{tech.map(item => <li>{item}</li>)}</ul>
+        <h2 className='techUsed'>This app was built using:</h2>
+        <ul className='techStack'>{tech.map(item => <li className='techStack'>{item}</li>)}</ul>
         </div>
     </div>
   )
